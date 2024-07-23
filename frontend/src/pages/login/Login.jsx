24386ecedf2864f78fd7d4d9ed6e3e10604f4739
SherikTZ <shervintursunzade@gmail.com>
2024-07-23 00:47:11 -0400

    Added login in functionality to frontend

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+const VITE_BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        `${VITE_BACKEND_API_URL}/login`,
+        { email, password },
+        { withCredentials: true }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
-          Username:
-          <input type="text" name="username" />
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label>
           Password:
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <button type="submit">Submit</button>
       </form>
